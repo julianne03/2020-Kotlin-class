826f968fbef023f5a160835d8cbc2ac1d63c0e26
Seungmin Yi <s2019w33@e-mirim.hs.kr>
2020-09-24 20:49:29 +0900

    #1 학교 홈페이지 가기, 전화 걸기, 갤러리 열기, 이전 페이지로 이동

---
+++ app/src/main/java/kr/hs/emirim/julianneyi/myandlab/Fourbutton2Activity.java
@@ -0,0 +1,53 @@
+package kr.hs.emirim.julianneyi.myandlab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Fourbutton2Activity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button mButton1;
+    private Button mButton2;
+    private Button mButton3;
+    private Button mButton4;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fourbutton2);
+
+        mButton1 = findViewById(R.id.button1);
+        mButton2 = findViewById(R.id.button2);
+        mButton3 = findViewById(R.id.button3);
+        mButton4 = findViewById(R.id.button4);
+
+        mButton1.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent;
+        switch (v.getId()) {
+            case R.id.button1 :
+                intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.e-mirim.hs.kr/"));
+                startActivity(intent);
+                break;
+            case R.id.button2 :
+                intent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:/01050443628"));
+                startActivity(intent);
+                break;
+            case R.id.button3 :
+                intent = new Intent(Intent.ACTION_VIEW, Uri.parse("content://media/interal/images/media"));
+                startActivity(intent);
+                break;
+            case R.id.button4 :
+                finish();
+                break;
+        }
+    }
+}
