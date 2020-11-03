1247cb668231022203ebacaf565ff48f765f520a
Seungmin Yi <s2019w33@e-mirim.hs.kr>
2020-11-03 20:44:17 +0900

    #5 여러가지 위젯 사용하기

---
+++ app/src/main/java/kr/hs/emirim/julianneyi/myandlab/chap04widget/PetsActivity.kt
@@ -0,0 +1,12 @@
+package kr.hs.emirim.julianneyi.myandlab.chap04widget
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kr.hs.emirim.julianneyi.myandlab.R
+
+class PetsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pets)
+    }
+}
